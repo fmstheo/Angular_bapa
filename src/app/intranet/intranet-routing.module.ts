ffd520e20011e506a11a2573a896c947868632d0
exlineo <gabbadie@step-sa.fr>
2019-04-09 14:43:01 +0200

    Ajout d'un dashboard et de questions fréquentes pour les besoins de la formation

--- src/app/intranet/intranet-routing.module.ts
+++ src/app/intranet/intranet-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
+import { FaqComponent } from './faq/faq.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path:'', component:AccueilComponent}
+  {path:'', component:AccueilComponent, 
+    children:[
+      {path:'', component:DashboardComponent},
+      {path:'faq', component:FaqComponent}
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/intranet/intranet.module.ts
+++ src/app/intranet/intranet.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AccueilComponent } from './accueil/accueil.component';
 import { MenuComponent } from './menu/menu.component';
 import { IntranetRoutingModule } from './intranet-routing.module';
+import { FooterComponent } from './footer/footer.component';
+import { FaqComponent } from './faq/faq.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AccueilComponent, MenuComponent],
+  declarations: [AccueilComponent, MenuComponent, FooterComponent, FaqComponent, DashboardComponent],
   imports: [
     CommonModule,
     IntranetRoutingModule
