d59152d7b05fdd9855762abdecb6f6a38f63fadd
darkcast64 <bertrand.parrot@greta-sud-aquitaine.academy>
2019-04-09 08:51:58 +0200

    premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { Erreur404Component } from './erreur404/erreur404.component';
+import { TokenService } from './services/token.service';
+import { AuthService } from './services/auth.service';
+import { FormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    Erreur404Component
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [TokenService,AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
