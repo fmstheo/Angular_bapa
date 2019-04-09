2f9a96c0dd8e969d7815273a428c80c197f03cef
exlineo <gabbadie@step-sa.fr>
2019-04-09 13:35:32 +0200

    Création d'un module enfant

---
+++ src/app/intranet/intranet-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+
+const routes: Routes = [
+  {path:'', component:AccueilComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class IntranetRoutingModule { }
--- src/app/intranet/intranet.module.ts
+++ src/app/intranet/intranet.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccueilComponent } from './accueil/accueil.component';
 import { MenuComponent } from './menu/menu.component';
+import { IntranetRoutingModule } from './intranet-routing.module';
 
 @NgModule({
   declarations: [AccueilComponent, MenuComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IntranetRoutingModule
   ]
 })
 export class IntranetModule { }
