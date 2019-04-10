e9bec6c2bd4d9b11e0aee09195894d1febcd50f1
darkcast64 <bertrand.parrot@greta-sud-aquitaine.academy>
2019-04-10 15:58:33 +0200

    troisieme commit

--- src/app/intranet/conges/conges.module.ts
+++ src/app/intranet/conges/conges.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { CongesRoutingModule } from './conges-routing.module';
+
 import { DdeCongesComponent } from './dde-conges/dde-conges.component';
 import { ConsultCongesComponent } from './consult-conges/consult-conges.component';
 import { CongesExceptionnelsComponent } from './conges-exceptionnels/conges-exceptionnels.component';
@@ -18,7 +18,7 @@ import { AccueilCongesComponent } from './accueil-conges/accueil-conges.componen
 
   imports: [
     CommonModule,
-    CongesRoutingModule,
+    
     FormsModule,
     HttpClientModule
   ],
--- src/app/intranet/intranet-routing.module.ts
+++ src/app/intranet/intranet-routing.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { MenuComponent } from './menu/menu.component';
+import { DdeCongesComponent } from './conges/dde-conges/dde-conges.component';
 
 
 
 const routes: Routes = [
   {path:'',component:AccueilComponent,
   children:[
-    {path:'conges',component:CongesComponent}
+    {path:'conges',component:DdeCongesComponent}
   ]
   
-  }
+  }]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
