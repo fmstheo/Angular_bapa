51ea8e5226f8b3721c67204c2ca0ecae8a8994ce
Théo <theo.guillaumin@fms-ea.com>
2019-04-11 15:38:36 +0200

    ajout code Franck

--- src/app/intranet/manager/manager.component.ts
+++ src/app/intranet/manager/manager.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ManagerService } from '../services/manager.service';
 
 @Component({
   selector: 'app-manager',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerComponent implements OnInit {
 
-  constructor() { }
+  constructor(public managerService:ManagerService) { }
 
   ngOnInit() {
   }
